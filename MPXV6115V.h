c95e3a597d5470740b362289b65c77fc7b5301b9
tunataskintuna35 <tunataskintuna35@gmail.com>
2015-12-14 11:22:48 +0200

    Create MPXV6115V.h

---
+++ MPXV6115V.h
@@ -0,0 +1,14 @@
+#ifndef MPXV6115V_h
+#define MPXV6115V_h
+
+#include <Arduino.h>
+class MPXV6115V
+{
+    public:
+      MPXV6115V(int pin);
+      float hesap();
+	  boolean fault();
+    private:
+      int _pin;
+};
+#endif

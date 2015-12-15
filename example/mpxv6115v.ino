8bff84608cef94d21487931dd8b54f2a2dabeb5a
tunataskintuna35 <tunataskintuna35@gmail.com>
2015-12-15 15:16:37 +0200

    Update mpxv6115v.ino

--- example/mpxv6115v.ino
+++ example/mpxv6115v.ino
@@ -1,13 +1,56 @@
 #include "MPXV6115V.h"
-int vacuum 
-Serial.begin(9600);
+#include "DRV8833.h"
+#include "Arduino.h"
+#include "EEPROM.h"
+#include "MAX7219.h"
+
+const int vacuum=A0;
+const int temp=A1;
+const int batt=A2;
+const int up=2;
+const int down=4;
+
+float vakum; 
+int setting=250;
+int tolerance=25;
+
+
+
 
 void setup()
-{
-	
+{	
+		  pinMode(up,INPUT);
+		  pinMode(down,INPUT);
+		  
+			Serial.begin(9600);
+		    while(!Serial);
+		  driver.attachMotorA(inputA1,inputA2);
+		  Serial.println("hazir");
+		  Serial.println("\n");
+		  
+
 }
 
 void loop()
 {
+			
+	// BUTON İLE ARTTIRMA AZALTMA  
+	// digitalRead(up);
+	// digitalRead(down);
+
+ 
+	while((vakum<setting-tolerance) || !(vakum>(setting+tolerance)) )
+	{
+		
+		driver.motorAForward();
+		vakum=analogRead(A0);
+			
+				
+		
+	}
+	driver.motorAStop();
+	
+	
+	
 	
 }

07dfb7a2cc4d034ade7cafd6f1a5d45b1a4ff7ae
tunataskintuna35 <tunataskintuna35@gmail.com>
2015-12-15 20:34:03 +0200

    Update mpxv6115v.ino

--- example/mpxv6115v.ino
+++ example/mpxv6115v.ino
@@ -3,27 +3,23 @@
 #include "Arduino.h"
 #include "EEPROM.h"
 #include "MAX7219.h"
-
+//************************************************NESNE ATAMALARI***********************************************************
 LedControl lc=LedControl(12,11,10,4);				//MAX7219 segment pin ve nesne atamaları
 DRV8833 driver=DRV8833();					//DRV8833 sınıfı bir nesne olarak tanımlandırıldı
 MPXV6115V mpxv6115v(A0);					//MPXV6115V nesne ataması yapıldı
 
-
+//************************************************PİN ATAMALARI*************************************************************
 
 const int vacuum=A0;						//vakum analog pin
 const int temp=A1;						//temperature analog pin
 const int batt=A2;						//battery analog pin
 const int up=2;							//setting değerini yukarı arttırmak için
 const int down=3;						//setting değerini aşağı azaltmak için 
-
+//*************************************************DEĞİŞKENLER**************************************************************
 float vakum; 
 int setting=250;
 int tolerance=25;
-
-
 float temp_voltage,degreeC,degreeF;				//Isı değişkenlerinin tanımlanması 
-
-
 const unsigned long delaytime=250;
 
 
